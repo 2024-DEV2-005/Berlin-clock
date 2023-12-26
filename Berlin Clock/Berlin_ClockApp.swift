4b05b6f8a359ce8cda70017767775d55aceecfc4
KKAR <k.karim@easi.net>
2023-12-26 17:55:22 +0100

    Initial Commit

---
+++ Berlin Clock/Berlin_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Berlin_ClockApp.swift
+//  Berlin Clock
+//
+//  Created by KKAR on 26/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Berlin_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
