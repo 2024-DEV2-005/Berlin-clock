44fd940f7c75f4aae6593395068814992f4adfde
KKAR <k.karim@easi.net>
2023-12-27 17:16:37 +0100

    Add Views components circle for seconds and rectangles for blocks

---
+++ Berlin Clock/Views/Components/lightsBlockView.swift
@@ -0,0 +1,19 @@
+//
+//  lightsBlockView.swift
+//  Berlin Clock
+//
+//  Created by KKAR on 27/12/2023.
+//
+
+import SwiftUI
+
+struct lightsBlockView: View {
+    var body: some View {
+        Rectangle()
+            .frame(width: 60, height: 40 )
+    }
+}
+
+#Preview {
+    lightsBlockView()
+}
---
+++ Berlin Clock/Views/Components/secondsView.swift
@@ -0,0 +1,20 @@
+//
+//  secondsView.swift
+//  Berlin Clock
+//
+//  Created by KKAR on 27/12/2023.
+//
+
+import SwiftUI
+
+struct secondsView: View {
+    var body: some View {
+        Circle()
+            .frame(width: 100)
+            
+    }
+}
+
+#Preview {
+    secondsView()
+}
