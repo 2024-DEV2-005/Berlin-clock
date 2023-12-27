669f87d8e25198b7929f03e60ffacc04333860c9
KKAR <k.karim@easi.net>
2023-12-27 18:44:52 +0100

    Fix ForEach warning

--- Berlin Clock/ViewModel/BerlinClockViewModel.swift
+++ Berlin Clock/ViewModel/BerlinClockViewModel.swift
@@ -12,13 +12,12 @@ class BerlinClockViewModel: ObservableObject {
     @Published var berlinClock: BerlinClock
     let converter: BerlinClockConverter
     let dateProvider: () -> Date
-    private var timer: Timer?
     
     init(dateProvider: @escaping () -> Date, converter: BerlinClockConverter) {
         self.dateProvider = dateProvider
         self.converter = converter
         self.berlinClock = converter.convert(date: self.dateProvider())
-        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             self.update()
         }
     }
