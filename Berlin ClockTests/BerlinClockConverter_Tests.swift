bebb7401fd2c484e892b35c938fb8449a2441c1a
KKAR <k.karim@easi.net>
2023-12-27 18:28:47 +0100

    Remove unused functions in tests

--- Berlin ClockTests/BerlinClockConverter_Tests.swift
+++ Berlin ClockTests/BerlinClockConverter_Tests.swift
@@ -13,15 +13,7 @@ import XCTest
 // Testing Structure: Given, When, Then
 
 final class BerlinClockConverter_Tests: XCTestCase {
-    
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
+
     func test_BerlinClockConverter_seconds_shouldBeFalse() {
         
         // Given
@@ -221,5 +213,4 @@ final class BerlinClockConverter_Tests: XCTestCase {
             XCTAssertEqual($0, Light.yellow)
         }
     }
-    
 }
